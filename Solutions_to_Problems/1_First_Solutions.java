75901fb503075819d5d964b5a3e7a0dbcacda09e
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-24 21:50:34 +0300

    Create 1_First_Solutions.java

---
+++ Solutions_to_Problems/1_First_Solutions.java
@@ -0,0 +1,71 @@
+Here are some beginner problems in Java that you can practice to sharpen your skills. The problems range from simple tasks to slightly more complex ones that involve fundamental concepts in programming. 
+
+1. **Print 'Hello' on screen and your name on a separate line.**
+```java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+        System.out.println("Your Name");
+    }
+}
+```
+[Source 2](https://www.w3resource.com/java-exercises/basic/index.php)
+
+2. **Print the sum of two numbers.**
+```java
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 74, num2 = 36, sum;
+        sum = num1 + num2;
+        System.out.println("Sum: "+sum);
+    }
+}
+```
+[Source 2](https://www.w3resource.com/java-exercises/basic/index.php)
+
+3. **Divide two numbers and print them on the screen.**
+```java
+public class Main {
+    public static void main(String[] args) {
+        int num1 = 50, num2 = 3, result;
+        result = num1 / num2;
+        System.out.println("Result: "+result);
+    }
+}
+```
+[Source 2](https://www.w3resource.com/java-exercises/basic/index.php)
+
+4. **Print the results of the following operations.**
+```java
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(-5 + 8 * 6);
+        System.out.println((55+9) % 9);
+        System.out.println(20 + -3*5 / 8);
+        System.out.println(5 + 15 / 3 * 2 - 8 % 3);
+    }
+}
+```
+[Source 2](https://www.w3resource.com/java-exercises/basic/index.php)
+
+5. **Take two numbers as input and display the product of two numbers.**
+```java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Input first number: ");
+        int num1 = scanner.nextInt();
+        System.out.print("Input second number: ");
+        int num2 = scanner.nextInt();
+        System.out.println(num1 + " x " + num2 + " = " + num1*num2);
+    }
+}
+```
+[Source 2](https://www.w3resource.com/java-exercises/basic/index.php)
+
+Try to understand the logic behind each problem and its solution. This will help you improve your problem-solving skills and make you more comfortable with Java programming.
+
+
+  
