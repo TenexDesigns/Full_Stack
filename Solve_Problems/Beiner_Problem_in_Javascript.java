bdb005ae449ff0745f22fd5b5a09f85810363ba3
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-08-24 21:45:24 +0300

    Create Beiner_Problem_in_Javascript.java

---
+++ Solve_Problems/Beiner_Problem_in_Javascript.java
@@ -0,0 +1,44 @@
+Certainly! Here are some beginner-level programming problems in JavaScript that can help you practice and improve your coding skills:
+
+1. **Hello World**: Just like in any language, start with printing "Hello, World!" to the console.
+
+2. **Sum of Two Numbers**: Write a program that takes two numbers as input and outputs their sum.
+
+3. **Factorial**: Calculate the factorial of a given number using loops or recursion.
+
+4. **Fibonacci Series**: Generate and print the Fibonacci sequence up to a certain number of terms.
+
+5. **Prime Numbers**: Write a program that checks if a given number is prime or not.
+
+6. **Palindrome Check**: Determine if a given string is a palindrome.
+
+7. **FizzBuzz**: Print numbers from 1 to 100. For multiples of 3, print "Fizz," for multiples of 5, print "Buzz," and for multiples of both 3 and 5, print "FizzBuzz."
+
+8. **Array Manipulation**: Perform basic operations on arrays like finding the largest element, reversing an array, or finding the sum of elements.
+
+9. **String Reversal**: Reverse a given string without using built-in functions.
+
+10. **Factorial of Large Numbers**: Calculate the factorial of large numbers using BigInt.
+
+11. **GCD and LCM**: Find the greatest common divisor (GCD) and least common multiple (LCM) of two numbers.
+
+12. **Swap Two Variables**: Swap the values of two variables without using a temporary variable.
+
+13. **Binary to Decimal Conversion**: Convert a binary number to its decimal equivalent.
+
+14. **Anagrams**: Check if two strings are anagrams (contain the same characters in different orders).
+
+15. **Count Characters**: Count the number of occurrences of each character in a string.
+
+16. **Armstrong Number**: Check if a number is an Armstrong number (sum of its own digits, each raised to the power of the number of digits).
+
+17. **Print Patterns**: Print various patterns using loops, like triangles and rectangles.
+
+18. **Linear Search**: Implement a linear search algorithm to find an element in an array.
+
+19. **Bubble Sort**: Implement the bubble sort algorithm to sort an array.
+
+20. **Calculator**: Build a simple calculator that can perform basic arithmetic operations.
+
+These problems cover a wide range of fundamental concepts in JavaScript. Working on these problems will help you become more comfortable with the language and build your problem-solving skills. Remember that consistent practice is the key to improving your programming abilities.
+
